41fd556ea0d748d0d0af3e456d4ddf94f665984d
nhanlucky1477 <leluthiennhan31@gmail.com>
2022-11-26 12:24:08 +0700

    API 26/11

--- control/customer/API/SelectDetailById.php
+++ control/customer/API/SelectDetailById.php
@@ -6,6 +6,7 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-type
 
 include_once("../../../config/db.class.php");
 include_once("../../../model/dt_order.php");
+include_once("../../../model/product.php");
 
 $data = json_decode(file_get_contents("php://input"));
 $orderID = $data->orderID;
@@ -21,12 +22,22 @@ if($num > 0){
 
     while($row = $read->fetch(PDO::FETCH_ASSOC)){
         extract($row);
+        //lay product
+        $product = new Product("","","","","","","","","","");
+        $read2 = $product->SelectById($productID);
+        $row2 = $read2->fetch(PDO::FETCH_ASSOC);
+        extract($row2);
 
+        // tinh tong gia
+        $totalPrice = $quantity*$price;
         $item = array(
             'orderID' => $orderID,
             'productID' => $productID,
             'quantity' => $quantity,
-            'price' => $price
+            'price' => $price,
+            'productName' => $productName,
+            'image' => $image,
+            'totalPrice' => $totalPrice
 
         );
         array_push($list['dt_order'], $item);
